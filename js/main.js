0b152075fbc1cc0493e9f3256869d992db6e4913
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-08-29 14:06:28 +0900

    JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-const iconContainer = document.querySelector('.icon-group');
-const btnContainer = document.querySelector('.btn-group');
-iconContainer.addEventListener('click', event => {
+const navEl = document.querySelector('nav');
+const clothesEls = document.querySelectorAll('.clothes');
+navEl.addEventListener('click', event => {
   const filter = event.target.dataset.filter;
-  console.log(filter);
+  // console.log(filter);
+  if (filter === null) {
+    return;
+  }
+  clothesEls.forEach(function(clothesEl) {
+    // console.log(clothesEl.dataset.type);
+    if(filter === clothesEl.dataset.type) {
+      clothesEl.classList.remove('invisible');
+    } else {
+      clothesEl.classList.add('invisible');
+    }
+  });
 });
